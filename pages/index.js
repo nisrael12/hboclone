fe989586052b0ae356c925ed17c4dbdf84d0dfe5
Navi Israel <dev@naviisrael.com>
2021-10-13 00:40:07 -0500

    key added

--- pages/index.js
+++ pages/index.js
@@ -19,31 +19,37 @@ export default function Home() {
       <FeaturedMedia />
       <MediaRow
         title="Movies"
+        key="movies"
         type="large-v"
         endpoint="discover/movie?sort_by=popularity.desc&primary_release_year=2021"
       />
       <MediaRow
         title="Series"
+        key="series"
         type="small-h"
         endpoint="discover/tv?&primary_release_year=2021"
       />
       <MediaRow
         title="Action"
+        key="action"
         type="small-v"
         endpoint="discover/movie?with_genres=28&primary_release_year=2021"
       />
       <MediaRow
         title="Horror"
+        key="horror"
         type="large-h"
         endpoint="discover/movie?with_genres=27&primary_release_year=2021"
       />
       <MediaRow
         title="Animations"
+        key="animations"
         type="large-v"
         endpoint="discover/movie?with_genres=16&primary_release_year=2021"
       />
       <MediaRow
         title="Sci-fi"
+        key="sci-fi"
         type="small-h"
         endpoint="discover/movie?with_genres=878&primary_release_year=2021"
       />
